26d9143cf34cbae3bb2b52e1d2197c4feb438fe2
chobits tai <chobits@itri.org.tw>
2024-01-25 23:09:26 +0800

    cam intrinsic from opencv cali

--- feature_tracker.py
+++ feature_tracker.py
@@ -10,72 +10,6 @@ from geometry_msgs.msg import Point32, Vector3
 import std_msgs.msg
 import time
 
-class FeatureTrackerDrawer:
-
-    lineColor = (200, 0, 200)
-    pointColor = (0, 0, 255)
-    circleRadius = 2
-    maxTrackedFeaturesPathLength = 30
-    # for how many frames the feature is tracked
-    trackedFeaturesPathLength = 10
-
-    trackedIDs = None
-    trackedFeaturesPath = None
-
-    def onTrackBar(self, val):
-        FeatureTrackerDrawer.trackedFeaturesPathLength = val
-        pass
-
-    def trackFeaturePath(self, features):
-
-        newTrackedIDs = set()
-        for currentFeature in features:
-            currentID = currentFeature.id
-            newTrackedIDs.add(currentID)
-
-            if currentID not in self.trackedFeaturesPath:
-                self.trackedFeaturesPath[currentID] = deque()
-
-            path = self.trackedFeaturesPath[currentID]
-
-            path.append(currentFeature.position)
-            while(len(path) > max(1, FeatureTrackerDrawer.trackedFeaturesPathLength)):
-                path.popleft()
-
-            self.trackedFeaturesPath[currentID] = path
-
-        featuresToRemove = set()
-        for oldId in self.trackedIDs:
-            if oldId not in newTrackedIDs:
-                featuresToRemove.add(oldId)
-
-        for id in featuresToRemove:
-            self.trackedFeaturesPath.pop(id)
-
-        self.trackedIDs = newTrackedIDs
-
-    def drawFeatures(self, img):
-
-        cv2.setTrackbarPos(self.trackbarName, self.windowName, FeatureTrackerDrawer.trackedFeaturesPathLength)
-
-        for featurePath in self.trackedFeaturesPath.values():
-            path = featurePath
-
-            for j in range(len(path) - 1):
-                src = (int(path[j].x), int(path[j].y))
-                dst = (int(path[j + 1].x), int(path[j + 1].y))
-                cv2.line(img, src, dst, self.lineColor, 1, cv2.LINE_AA, 0)
-            j = len(path) - 1
-            cv2.circle(img, (int(path[j].x), int(path[j].y)), self.circleRadius, self.pointColor, -1, cv2.LINE_AA, 0)
-
-    def __init__(self, trackbarName, windowName):
-        self.trackbarName = trackbarName
-        self.windowName = windowName
-        cv2.namedWindow(windowName)
-        cv2.createTrackbar(trackbarName, windowName, FeatureTrackerDrawer.trackedFeaturesPathLength, FeatureTrackerDrawer.maxTrackedFeaturesPathLength, self.onTrackBar)
-        self.trackedIDs = set()
-        self.trackedFeaturesPath = dict()
-
 def getMesh(camSocket, calibData):
     M1 = np.array(calibData.getCameraIntrinsics(camSocket, 1280, 720))
     d1 = np.array(calibData.getDistortionCoefficients(camSocket))
@@ -117,108 +51,6 @@ def getMesh(camSocket, calibData):
     mesh = list(map(tuple, mesh0))
 
     return mesh, meshWidth, meshHeight, new_M1
-
-#def prepare_features_pp(ts, prv_ts, features, prv_features):
-#    l_ts = features_left_msg.getTimestamp().total_seconds()
-#    dt = l_ts - l_prv_ts
-#    tracked_ids = set()
-#    if pp_msg is None:
-#        pp_msg = PointCloud()
-#        pp_msg.header = std_msgs.msg.Header()
-#        pp_msg.header.stamp = rospy.Time.from_sec(base_ts + l_ts)
-#        pp_msg.header.frame_id = 'map'
-#        pp_msg.channels = [ChannelFloat32()]*6
-#    for feature in trackedFeaturesLeft:
-#        tracked_ids.add(feature.id)
-#        x_u = feature.position.x / l_un_fx - l_un_cx / l_un_fx
-#        y_u = feature.position.y / l_un_fy - l_un_cy / l_un_fy
-#        pp_msg.points.append(Point32(x_u, y_u, 1))
-#        pp_msg.channels[0].values.append(feature.id)
-#        pp_msg.channels[1].values.append(0)
-#        pp_msg.channels[2].values.append(feature.position.x)
-#        pp_msg.channels[3].values.append(feature.position.y)
-#        vx = 0
-#        vy = 0
-#        if feature.id in l_tracked_features:
-#            vx = (x_u - l_tracked_features[feature.id][0]) / dt
-#            vy = (y_u - l_tracked_features[feature.id][1]) / dt
-#        pp_msg.channels[4].values.append(vx)
-#        pp_msg.channels[5].values.append(vy)
-#    pp_pub.publish(pp_msg)
-#    l_prv_ts = l_ts
-#    no_track_ids = l_tracked_features.keys() - tracked_ids
-#    for id in no_track_ids:
-#        del l_tracked_features[id]
-
-def create_features_pp(l_prv_features_msg, r_prv_features_msg, l_latest_features_msg, r_latest_features_msg, base_ts, l_un_fx, l_un_fy, l_un_cx, l_un_cy, r_un_fx, r_un_fy, r_un_cx, r_un_cy):
-    pp_msg = PointCloud()
-    pp_msg.header = std_msgs.msg.Header()
-    pp_msg.header.stamp = rospy.Time.from_sec(base_ts + l_latest_features_msg.getTimestamp().total_seconds())
-    pp_msg.header.frame_id = 'map'
-    pp_msg.channels = [ChannelFloat32()]*6
-
-    prv_features = dict()
-    if l_prv_features_msg is not None:
-        for feature in l_prv_features_msg.trackedFeatures:
-            prv_features[feature.id] = feature.position
-        dt = l_latest_features_msg.getTimestamp().total_seconds() - l_prv_features_msg.getTimestamp().total_seconds()
-    print("left")
-    for feature in l_latest_features_msg.trackedFeatures:
-        print(feature.id)
-        x_u = feature.position.x / l_un_fx - l_un_cx / l_un_fx
-        y_u = feature.position.y / l_un_fy - l_un_cy / l_un_fy
-        pp_msg.points.append(Point32(x_u, y_u, 1))
-        pp_msg.channels[0].values.append(feature.id)
-        pp_msg.channels[1].values.append(0)
-        pp_msg.channels[2].values.append(feature.position.x)
-        pp_msg.channels[3].values.append(feature.position.y)
-        vx = 0
-        vy = 0
-        if feature.id in prv_features:
-            vx = (x_u - prv_features[feature.id].x) / dt
-            vy = (y_u - prv_features[feature.id].y) / dt
-        pp_msg.channels[4].values.append(vx)
-        pp_msg.channels[5].values.append(vy)
-
-    prv_features = dict()
-    if r_prv_features_msg is not None:
-        for feature in r_prv_features_msg.trackedFeatures:
-            prv_features[feature.id] = feature.position
-        dt = r_latest_features_msg.getTimestamp().total_seconds() - r_prv_features_msg.getTimestamp().total_seconds()
-    print("right")
-    for feature in r_latest_features_msg.trackedFeatures:
-        print(feature.id)
-        x_u = feature.position.x / r_un_fx - r_un_cx / r_un_fx
-        y_u = feature.position.y / r_un_fy - r_un_cy / r_un_fy
-        pp_msg.points.append(Point32(x_u, y_u, 1))
-        pp_msg.channels[0].values.append(feature.id)
-        pp_msg.channels[1].values.append(1)
-        pp_msg.channels[2].values.append(feature.position.x)
-        pp_msg.channels[3].values.append(feature.position.y)
-        vx = 0
-        vy = 0
-        if feature.id in prv_features:
-            vx = (x_u - prv_features[feature.id].x) / dt
-            vy = (y_u - prv_features[feature.id].y) / dt
-        pp_msg.channels[4].values.append(vx)
-        pp_msg.channels[5].values.append(vy)
-
-    return pp_msg
-
-def create_pp_msg(ts, cam_id, feature_id, cur_un_pts, x, y, vx, vy):
-    pp_msg = PointCloud()
-    pp_msg.header = std_msgs.msg.Header()
-    pp_msg.header.stamp = rospy.Time.from_sec(ts)
-    pp_msg.header.frame_id = 'map'
-    pp_msg.channels = [ChannelFloat32()]*6
-    pp_msg.points.append(Point32(cur_un_pts[0], cur_un_pts[1], 1))
-    pp_msg.channels[0].values.append(feature_id)
-    pp_msg.channels[1].values.append(cam_id)
-    pp_msg.channels[2].values.append(x)
-    pp_msg.channels[3].values.append(y)
-    pp_msg.channels[4].values.append(vx)
-    pp_msg.channels[5].values.append(vy)
-    return pp_msg
 
 base_ts = time.time() - dai.Clock.now().total_seconds()
 
@@ -263,8 +95,8 @@ monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
 monoRight.setFps(20)
 monoRight.setCamera("right")
 
-featureTrackerLeft.initialConfig.setNumTargetFeatures(32)
-featureTrackerRight.initialConfig.setNumTargetFeatures(32)
+featureTrackerLeft.initialConfig.setNumTargetFeatures(16*2)
+featureTrackerRight.initialConfig.setNumTargetFeatures(16*2)
 
 # enable ACCELEROMETER_RAW at 500 hz rate
 imu.enableIMUSensor(dai.IMUSensor.ACCELEROMETER_RAW, 250)
@@ -284,6 +116,7 @@ depth.setLeftRightCheck(True)
 depth.setExtendedDisparity(False)
 depth.setSubpixel(False)
 depth.setDepthAlign(dai.RawStereoDepthConfig.AlgorithmControl.DepthAlign.RECTIFIED_LEFT)
+depth.setAlphaScaling(0)
 
 # Linking
 monoLeft.out.link(depth.left)
@@ -309,21 +142,31 @@ featureTrackerRight.setHardwareResources(numShaves, numMemorySlices)
 
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
-    calibData = device.readCalibration()
-    intri = calibData.getCameraIntrinsics(dai.CameraBoardSocket.CAM_B, 640, 400)
-    fx = intri[0][0]
-    cx = intri[0][2]
-    fy = intri[1][1]
-    cy = intri[1][2]
+    #calibData = device.readCalibration()
+    #intri = calibData.getCameraIntrinsics(dai.CameraBoardSocket.CAM_B, 640, 400)
+    #fx = intri[0][0]
+    #cx = intri[0][2]
+    #fy = intri[1][1]
+    #cy = intri[1][2]
+    #print("left cam", fx ,fy, cx, cy)
+    fx = 4.2007635451376063e+02
+    cx = 3.0906091871893943e+02
+    fy = 4.1970786993146550e+02
+    cy = 2.0014588000163775e+02
     l_inv_k11 = 1.0 / fx
     l_inv_k13 = -cx / fx
     l_inv_k22 = 1.0 / fy
     l_inv_k23 = -cy / fy
-    intri = calibData.getCameraIntrinsics(dai.CameraBoardSocket.CAM_C, 640, 400)
-    fx = intri[0][0]
-    cx = intri[0][2]
-    fy = intri[1][1]
-    cy = intri[1][2]
+    #intri = calibData.getCameraIntrinsics(dai.CameraBoardSocket.CAM_C, 640, 400)
+    #fx = intri[0][0]
+    #cx = intri[0][2]
+    #fy = intri[1][1]
+    #cy = intri[1][2]
+    #print("right cam", fx ,fy, cx, cy)
+    fx = 4.1911737221609309e+02
+    cx = 3.1094101439493375e+02
+    fy = 4.1911302233054698e+02
+    cy = 1.9966812289233278e+02
     r_inv_k11 = 1.0 / fx
     r_inv_k13 = -cx / fx
     r_inv_k22 = 1.0 / fy
@@ -370,6 +213,7 @@ with dai.Device(pipeline) as device:
                     imu_pub.publish(imu_msg)
 
             if l_seq == r_seq == disp_seq:
+                #start_ts = time.time()
                 l_seq = -1
                 r_seq = -2
                 disp_seq = -3
@@ -395,7 +239,7 @@ with dai.Device(pipeline) as device:
                         for r_feature in r_features:
                             dy = y - r_feature.position.y
                             dx = x - disp - r_feature.position.x
-                            if dy * dy + dx * dx <= 36: #pair found
+                            if dy * dy + dx * dx <= 16: #pair found
                                 dt = features_ts - prv_features_ts
                                 vx = 0
                                 vy = 0
@@ -434,58 +278,4 @@ with dai.Device(pipeline) as device:
                 r_prv_features = {}
                 for feature in r_features:
                     r_prv_features[feature.id] = (r_inv_k11 * feature.position.x + r_inv_k13, r_inv_k22 * feature.position.y + r_inv_k23)
-#                seq = features_msg.getSequenceNum()
-#                features = features_msg.trackedFeatures
-#                ts = features_msg.getTimestamp().total_seconds()
-#                dt = ts - l_prv_ts
-#                tracked_ids = set()
-#                if pp_msg is None:
-#                    pp_msg = PointCloud()
-#                    pp_msg.header = std_msgs.msg.Header()
-#                    pp_msg.header.stamp = rospy.Time.from_sec(base_ts + l_ts)
-#                    pp_msg.header.frame_id = 'map'
-#                    pp_msg.channels = [ChannelFloat32()]*6
-#                for feature in trackedFeaturesLeft:
-#                    tracked_ids.add(feature.id)
-#                    x_u = feature.position.x / l_un_fx - l_un_cx / l_un_fx
-#                    y_u = feature.position.y / l_un_fy - l_un_cy / l_un_fy
-#                    pp_msg.points.append(Point32(x_u, y_u, 1))
-#                    pp_msg.channels[0].values.append(feature.id)
-#                    pp_msg.channels[1].values.append(0)
-#                    pp_msg.channels[2].values.append(feature.position.x)
-#                    pp_msg.channels[3].values.append(feature.position.y)
-#                    vx = 0
-#                    vy = 0
-#                    if feature.id in l_tracked_features:
-#                        vx = (x_u - l_tracked_features[feature.id][0]) / dt
-#                        vy = (y_u - l_tracked_features[feature.id][1]) / dt
-#                    pp_msg.channels[4].values.append(vx)
-#                    pp_msg.channels[5].values.append(vy)
-#                pp_pub.publish(pp_msg)
-#                l_prv_ts = l_ts
-#                no_track_ids = l_tracked_features.keys() - tracked_ids
-#                for id in no_track_ids:
-#                    del l_tracked_features[id]
-
-            #print("seq", features_left_msg.getSequenceNum(), features_right_msg.getSequenceNum())
-        #rightFeatureDrawer.trackFeaturePath(trackedFeaturesRight)
-        #rightFeatureDrawer.drawFeatures(rightFrame)
-
-        # Show the frame
-        #cv2.imshow(leftWindowName, leftFrame)
-        #cv2.imshow(rightWindowName, rightFrame)
-
-        #key = cv2.waitKey(1)
-        #if key == ord('q'):
-        #    break
-        #elif key == ord('s'):
-        #    if featureTrackerConfig.motionEstimator.type == dai.FeatureTrackerConfig.MotionEstimator.Type.LUCAS_KANADE_OPTICAL_FLOW:
-        #        featureTrackerConfig.motionEstimator.type = dai.FeatureTrackerConfig.MotionEstimator.Type.HW_MOTION_ESTIMATION
-        #        print("Switching to hardware accelerated motion estimation")
-        #    else:
-        #        featureTrackerConfig.motionEstimator.type = dai.FeatureTrackerConfig.MotionEstimator.Type.LUCAS_KANADE_OPTICAL_FLOW
-        #        print("Switching to Lucas-Kanade optical flow")
-
-        #    cfg = dai.FeatureTrackerConfig()
-        #    cfg.set(featureTrackerConfig)
-        #    inputFeatureTrackerConfigQueue.send(cfg)
+                #print("total", len(pp_msg.points), "points, cost", time.time() - start_ts, "s")
